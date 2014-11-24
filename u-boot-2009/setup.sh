f4f4888028ebb317da33b8d06e2edc0a4322807f
finallyjustice <dongli.zhang0129@gmail.com>
2014-11-24 01:39:29 -0500

    remember which files are changed

--- u-boot-2009/setup.sh
+++ u-boot-2009/setup.sh
@@ -2,4 +2,9 @@
 # export ARCH=arm
 # make mx53loco_defconfig
 
+# files changed:
+# cpu/arm_cortexa8/config.mk
+# board/freescale/mx53_loco/mx53_loco.c dram_init()
+# lib_arm/Makefile
+
 sudo dd if=u-boot.imx of=/dev/mmcblk0 bs=512 seek=2
